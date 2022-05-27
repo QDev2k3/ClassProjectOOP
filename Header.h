5c46cb3eec821d359b916b67e6dd5f737ae5bfcf
Quốc Bủh <87589556+QuocDev2003@users.noreply.github.com>
2022-05-27 08:36:27 +0700

    Update Caption

--- Header.h
+++ Header.h
@@ -10,14 +10,14 @@ class Customer;
 
 class Tourist {
     protected:
-        string name,id,address;
-        char gender;
-        int age;
-        int placeType; string place; 
+        string name,id,address;   // Tên,CMND, Địa chỉ
+        char gender; // Giới tính
+        int age; // Tuổi
+        int placeType; string place; // placeType=1 : Nội địa, placeType=2: Nước ngoài
         string transport /*bus, plane, cruiseship, train*/;
-        int tier; string ticketTier; /* Regular vs VIP */
-        int days; 
-        int price;
+        int tier; string ticketTier; // loại vé
+        int days; // Số ngày du lịch
+        int price; // Phí du lịch
     public:
         void setName();
         void setID();
@@ -25,15 +25,15 @@ class Tourist {
         void setAge();
         void setAddress();
         void setPlace();
-        void setTier();
+        void setTier(); 
         void setDuration();
-        virtual void setPrice() = 0;
-        
+        virtual void setPrice() = 0;  // Hàm thuần ảo
+         
 };
 
 class Children: public Tourist {
     public:
-        void setPrice();
+        void setPrice();  // Set phí du lịch cho trẻ em
         friend class Customer;
         friend void PrintFile(ofstream &fileOut, Customer S);
         friend void ReadFile(ifstream &fileIn, Customer &C);
@@ -44,7 +44,7 @@ class Children: public Tourist {
 
 class Adult: public Tourist {
     public:
-        void setPrice();
+        void setPrice(); // Set phí du lịch cho người lớn
         friend class Customer;
         friend void PrintFile(ofstream &fileOut, Customer S);
         friend void ReadFile(ifstream &fileIn, Customer &C);
@@ -71,13 +71,13 @@ class Customer {
         friend void DeleteRecord();
 };
 
-void PrintFile(ofstream &fileOut, Customer S);
-void ReadFile(ifstream &fileIn, Customer &C);
-void VectorReadFile(ifstream &fileIn, vector<Customer> &list);
-void SortRecord();
-void ViewByFee();
-void ViewByName();
-void ViewRecord();
-void AddRecord();
-void SearchRecord();
-void DeleteRecord();
+void PrintFile(ofstream &fileOut, Customer S);  // In thông tin ra file
+void ReadFile(ifstream &fileIn, Customer &C);   // Đọc thông tin cho 1 khách hàng từ file
+void VectorReadFile(ifstream &fileIn, vector<Customer> &list);  // Đọc thông tin cho 1 mảng khách hàng từ file
+void ViewByFee();  // In ra danh sách theo tên A-Z
+void ViewByName(); // In ra danh sách theo phí du lịch
+void ViewRecord(); // In ra danh sách (byName / byFee)
+void AddRecord(); // Thêm 1 khách hàng mới vào danh sách
+void SortRecord(); // Sắp xếp lại danh sách, gọi sau khi thêm một khách hàng mới vào danh sách
+void SearchRecord();  // Tìm 1 khách hàng và in ra tất cả thông tin cần thiết
+void DeleteRecord(); // Xóa 1 khách hàng
